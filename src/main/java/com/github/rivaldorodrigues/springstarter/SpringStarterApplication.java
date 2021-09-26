5adc134145d0101f939fb441c8c812e2ce3f4c66
Rivaldo Rodrigues <hiva.rodrigues@gmail.com>
2021-09-26 19:21:11 -0300

    Initial project structure

---
+++ src/main/java/com/github/rivaldorodrigues/springstarter/SpringStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.rivaldorodrigues.springstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rivaldorodrigues/springstarter/SpringStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rivaldorodrigues.springstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
